92900354499414d9628e0146d03517d6b0326e06
alf-inc <alvinjian@gmail.com>
2026-02-03 00:46:24 -0500

    added .env

--- front_end/src/firebase/firebase.ts
+++ front_end/src/firebase/firebase.ts
@@ -4,15 +4,17 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore, doc, getDoc, setDoc, collection, addDoc, query, where, getDocs, deleteDoc } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
+
+
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAB010mSTZIEzhtwBAELxfpFNS-D8ZUHTI",
-  authDomain: "tradeup-301.firebaseapp.com",
-  projectId: "tradeup-301",
-  storageBucket: "tradeup-301.firebasestorage.app",
-  messagingSenderId: "829476476398",
-  appId: "1:829476476398:web:07b4b89c6d39eaf80b6aff",
-  measurementId: "G-N61WPHVQLS",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
