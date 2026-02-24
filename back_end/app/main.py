a02e1c263bd7319240c1060f8045a79992cb9e61
RyanWu27 <ryanwu0484@gmail.com>
2026-02-24 17:18:27 -0500

    US9.1 initial setup

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routes import matches
+from app.routes import matches, ratings
 
 app = FastAPI()
 app.include_router(matches.router)
+app.include_router(ratings.router)
